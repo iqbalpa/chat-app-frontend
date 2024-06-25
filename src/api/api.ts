0b3eaac40a05e420befff6bda8f9ef4ffcb204b9
iqbalpa <iqbalpahlevi16@gmail.com>
2024-06-26 05:54:01 +0700

    feat(auth): update return from api call

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,20 +4,22 @@ import { SignUpRequest, SignUpResponse, SignInRequest, SignInResponse } from "@/
 const BASE_URL: string = "http://localhost:3000";
 
 export const signup = async (data: SignUpRequest) => {
-	const res: SignUpResponse = await axios.post(`${BASE_URL}/auth/signup`, {
+	let res = await axios.post(`${BASE_URL}/auth/signup`, {
 		name: data.name,
 		email: data.email,
 		password: data.password,
 	});
-	console.log(`result:\n${res}`);
+	res = res.data;
+	console.log(`result:\n${JSON.stringify(res)}`);
 	return res;
 };
 
 export const signin = async (data: SignInRequest) => {
-	const res: SignInResponse = await axios.post(`${BASE_URL}/auth/signin`, {
+	let res = await axios.post(`${BASE_URL}/auth/signin`, {
 		email: data.email,
 		password: data.password,
 	});
+	res = res.data;
 	console.log(`result:\n${JSON.stringify(res)}`);
 	return res;
 };
