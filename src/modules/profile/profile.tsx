383e2eb286017959e5a817e30c3a47036c35be6d
iqbalpa <iqbalpahlevi16@gmail.com>
2024-06-27 09:44:27 +0700

    feat(profile): create profile page

---
+++ src/modules/profile/profile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ProfileModule: React.FC = () => {
+	return (
+		<div className="min-h-screen flex justify-center items-start mt-20">
+			<div className="flex flex-col gap-4 justify-center items-center h-full w-2/3 border-slate-500 border-[1px] rounded-xl px-5 pb-10 shadow-lg">
+				<div className="bg-sky-500 border-slate-500 border-[1px] shadow-2xl rounded-full p-5 min-h-20 min-w-20 flex justify-center items-center -m-10">
+					IP
+				</div>
+				<div className="flex flex-col justify-center items-center mt-10">
+					<p className="font-bold text-3xl">Iqbal Pahlevi</p>
+					<p className="text-neutral-700">iqbalpahlevi@gmail.com</p>
+				</div>
+				<p className="indent-8 text-justify">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit diam ac turpis sagittis
+					rutrum. Praesent pulvinar odio vitae rhoncus malesuada. Integer tempus suscipit semper. Etiam eros
+					velit, sollicitudin eu dignissim et, interdum nec ex. Morbi non sapien ornare, molestie urna vitae,
+					pulvinar velit. Aenean posuere elementum ipsum, a laoreet augue ultrices et. Vivamus et metus
+					auctor, mollis eros scelerisque, auctor lacus. Aenean elementum mollis neque, sed ornare purus
+					vehicula vitae.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default ProfileModule;
