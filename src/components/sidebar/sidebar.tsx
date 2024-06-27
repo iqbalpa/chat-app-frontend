c9581996dcfc12b6debc60eb14d01f94f0c1688a
iqbalpa <iqbalpahlevi16@gmail.com>
2024-06-27 08:33:36 +0700

    feat(sidebar): init shadcnui and create sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import UserItem from "../userItem/userItem";
+import Menu from "../menu/menu";
 
 const Sidebar: React.FC = () => {
 	return (
 		<div className="flex flex-col gap-4 w-[300px] border-r-2 min-h-screen p-4">
 			<UserItem />
-			<div>Menu Item 1</div>
-			<div>Menu Item 2</div>
-			<div className="grow">Menu Item 3</div>
-			<div>Settings</div>
+			<Menu />
 		</div>
 	);
 };
