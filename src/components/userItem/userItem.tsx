8a5443abaec0dd41694e485b7ec67cdf70fd60d3
iqbalpa <iqbalpahlevi16@gmail.com>
2024-06-28 10:26:59 +0700

    feat(sidebar): integrate user item with backend

--- src/components/userItem/userItem.tsx
+++ src/components/userItem/userItem.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/userStore";
 
 const UserItem: React.FC = () => {
+	const user = useSelector((state: RootState) => state.user.user);
+
 	return (
 		<div className="flex flex-row items-center gap-2 border rounded-lg p-4">
 			<div className="bg-sky-500 rounded-full min-h-12 min-w-12 flex justify-center items-center">
-				<p>IP</p>
+				<p>{user?.name.charAt(0).toUpperCase()}</p>
 			</div>
 			<div className="text-sm">
-				<p className="font-bold text-[16px]">Iqbal Pahlevi</p>
-				<p className="text-neutral-500 text-[12px]">iqbalpahlevi@gmail.com</p>
+				<p className="font-bold text-[16px]">{user?.name}</p>
+				<p className="text-neutral-500 text-[12px]">{user?.email}</p>
 			</div>
 		</div>
 	);
