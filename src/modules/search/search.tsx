0d7bb8bf18643ab3dbc56a647477987092600346
iqbalpa <iqbalpahlevi16@gmail.com>
2024-06-29 09:26:36 +0700

    feat(search): create search bar

--- src/modules/search/search.tsx
+++ src/modules/search/search.tsx
@@ -1,9 +1,16 @@
+import { Search } from "lucide-react";
 import React from "react";
 
 const SearchModule: React.FC = () => {
 	return (
-		<div>
-			<h1>search page</h1>
+		<div className="bg-sky-200 p-10 min-h-screen flex flex-col items-center justify-start">
+			<div className="flex flex-row w-full relative">
+				<Search size={24} className="absolute left-4 top-1/2 transform -translate-y-1/2 text-slate-500" />
+				<input
+					placeholder="Search user"
+					className="border-2 border-slate-300 active:border-slate-500 py-4 px-12 rounded-full grow"
+				/>
+			</div>
 		</div>
 	);
 };
