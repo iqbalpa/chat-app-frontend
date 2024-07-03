6e2a955c53e62ecbb3ccb5ca497824af605c1c1a
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-03 11:37:08 +0700

    feat(inbox): create bubble chat

--- src/modules/inbox/inbox.tsx
+++ src/modules/inbox/inbox.tsx
@@ -3,6 +3,24 @@
 import React, { useState, useRef, useEffect, ChangeEvent } from "react";
 import { ArrowLeft, SendHorizontal } from "lucide-react";
 
+interface Message {
+	name: string;
+	text: string;
+}
+
+const messages: Message[] = [
+	{ name: "Alice", text: "Hey everyone, how are you all doing?" },
+	{ name: "Me", text: "I am doing great, Alice! How about you?" },
+	{ name: "Alice", text: "I'm doing well, thanks for asking!" },
+	{ name: "Charlie", text: "Same here, enjoying the weather!" },
+	{ name: "Alice", text: "That's great to hear, Charlie!" },
+	{ name: "Me", text: "Anyone up for a game later?" },
+	{
+		name: "Alice",
+		text: "Hmm, that's a good idea. But, I think I cannot join you guys because I have a meeting this evening.",
+	},
+];
+
 const InboxModule: React.FC = () => {
 	const [message, setMessage] = useState<string>("");
 	const textareaRef = useRef<HTMLTextAreaElement>(null);
@@ -27,7 +45,22 @@ const InboxModule: React.FC = () => {
 			</div>
 
 			{/* chat space */}
-			<div className="grow"></div>
+			<div className="grow flex flex-col gap-1 p-2">
+				{messages.map((message, index) => (
+					<div key={index} className={`flex ${message.name === "Me" ? "justify-end" : "justify-start"}`}>
+						<div
+							className={`flex flex-col px-4 py-2 text-left ${
+								message.name === "Me"
+									? "bg-slate-300 text-right rounded-tl-lg rounded-bl-lg rounded-tr-lg"
+									: "bg-sky-300 rounded-tr-lg rounded-br-lg rounded-tl-lg"
+							}`}
+						>
+							<p className="text-xs font-bold">{message.name}</p>
+							<p>{message.text}</p>
+						</div>
+					</div>
+				))}
+			</div>
 
 			{/* chat input */}
 			<div className="flex flex-row items-center justify-center px-6 py-6 border-t-2 border-slate-200">
