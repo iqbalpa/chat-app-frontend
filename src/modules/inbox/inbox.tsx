8aa30b97c8ab282df9f66e15de8934a887c8eab5
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-04 11:04:41 +0700

    feat(inbox): send and receive message from backend works, but it called twice

--- src/modules/inbox/inbox.tsx
+++ src/modules/inbox/inbox.tsx
@@ -2,49 +2,42 @@
 
 import React, { useState, useRef, useEffect, ChangeEvent } from "react";
 import { ArrowLeft, SendHorizontal } from "lucide-react";
+import io, { Socket } from "socket.io-client";
 
 interface Message {
 	name: string;
 	text: string;
 }
 
-const messages: Message[] = [
-	{ name: "Alice", text: "Hey everyone, how are you all doing?" },
-	{ name: "Me", text: "I am doing great, Alice! How about you?" },
-	{ name: "Alice", text: "I'm doing well, thanks for asking!" },
-	{ name: "Charlie", text: "Same here, enjoying the weather!" },
-	{ name: "Alice", text: "That's great to hear, Charlie!" },
-	{ name: "Me", text: "Anyone up for a game later?" },
-	{
-		name: "Alice",
-		text: "Hmm, that's a good idea. But, I think I cannot join you guys because I have a meeting this evening.",
-	},
-	{ name: "Me", text: "Ah, that is so unfortunate" },
-	{ name: "Charlie", text: "How about tomorrow instead?" },
-	{ name: "Alice", text: "It would be great" },
-	{ name: "Me", text: "Ok, then see you tomorrow you all" },
-	{ name: "Charlie", text: "Sure, looking forward to it!" },
-	{ name: "Alice", text: "I'll make sure to be there!" },
-	{ name: "Me", text: "Awesome! Let's decide the time then." },
-	{ name: "Charlie", text: "How about 6 PM?" },
-	{ name: "Alice", text: "6 PM works for me." },
-	{ name: "Me", text: "Perfect! See you all at 6 PM tomorrow." },
-	{ name: "Charlie", text: "Great, see you all then!" },
-	{ name: "Alice", text: "See you tomorrow!" },
-];
-
 const InboxModule: React.FC = () => {
+	const [messages, setMessages] = useState<Message[]>([]);
 	const [message, setMessage] = useState<string>("");
+	const [socket, setSocket] = useState<Socket | null>(null);
 	const textareaRef = useRef<HTMLTextAreaElement>(null);
 
 	useEffect(() => {
-		if (textareaRef.current) {
-			textareaRef.current.style.height = "auto";
-			textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-		}
-	}, [message]);
+		const newSocket = io("http://localhost:3000");
+		setSocket(newSocket);
+		newSocket.on("message", (message: Message) => {
+			console.log("from server: ", message);
+			setMessages((prevMessages) => [...prevMessages, message]);
+		});
+		return () => {
+			if (socket) {
+				socket.disconnect();
+			}
+		};
+	}, []);
 
 	const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value);
+
+	const sendMessage = () => {
+		if (socket) {
+			const newMessage: Message = { name: "Me", text: message };
+			socket.emit("createMessage", newMessage);
+			setMessage("");
+		}
+	};
 
 	return (
 		<div className="bg-white flex flex-col w-full min-h-screen">
@@ -85,7 +78,10 @@ const InboxModule: React.FC = () => {
 					placeholder="Type your message..."
 					rows={1}
 				/>
-				<div className="absolute rounded-lg right-[21rem] p-1 hover:cursor-pointer hover:bg-slate-500/30 duration-100">
+				<div
+					onClick={sendMessage}
+					className="absolute rounded-lg right-[21rem] p-1 hover:cursor-pointer hover:bg-slate-500/30 duration-100"
+				>
 					<SendHorizontal />
 				</div>
 			</div>
